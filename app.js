c42573946d8f4390213f01767b39baf7bf878724
Fabien RENAUD <frd.frenaud@gmail.com>
2017-10-28 23:17:42 +0200

    cpu collect
    cpu collect and publish done

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 const mqtt = require('mqtt');
 const client  = mqtt.connect('http://192.168.1.100');
-const cpu = require('cpu-stats');
-//const stats = require('sysstats')();
+var stats = require('sysstats')();
+var cpuCounter = stats.cpus(),
+    ramCounter = stats.mem();
+var cpuPercent = 0,
+    cpuIdle = 0,
+    cpuTotal = 0,
+    cpuIdlep = 0,
+    cpuTotalp = 0,
+    cpuIdled = 0,
+    cpuTotald = 0;
 
-function getCPU() {
-    cpu(500, function(error, result) {
-        if(error) return console.error('Error: ', error);
-        console.info(result[0]['cpu']);
-        client.publish('test', result[0]['cpu'].toString());
+function getCPU(send) {
+    // refresh cpuCounter
+    cpuCounter = stats.cpus();
+    cpuCounter.forEach(function(e) {
+        cpuIdle += e.times.idle;
+        cpuTotal += e.times.user + e.times.nice + e.times.sys + e.times.idle + e.times.irq;
     })
-    setTimeout(function() { getCPU(); }, 1000);
+
+    // calculate difference between updated and previous values
+    cpuIdled = cpuIdle - cpuIdlep;
+    cpuTotald = cpuTotal - cpuTotalp;
+
+    // attribuates new values to previous values
+    cpuIdlep = cpuIdle;
+    cpuTotalp = cpuTotal;
+
+    // calculate CPU usage in percentage, with 2 decimals
+    cpuPercent = (((cpuTotald - cpuIdled)/cpuTotald)*100).toFixed(2);
+
+    // publish data on the broker
+    if(send == true) {
+        client.publish('cpu', cpuPercent.toString());
+    }
+
+    // callback getCPU()
+    setTimeout(function() { getCPU(true); }, 1000);
 }
 
 client.on('connect', () => {
     client.publish('test', 'New publisher !')
 })
 
-getCPU();
-//stats.cpus();
+getCPU(false);
