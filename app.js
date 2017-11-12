694c64d0a9340f209b300a6feb9d08d63232334d
Fabien RENAUD <frd.frenaud@gmail.com>
2017-11-12 01:39:42 +0100

    topic update
    topic updated
    comments updated

--- app.js
+++ app.js
@@ -17,47 +17,49 @@ const ip = address.ip();
 const mac = address.mac(function(err, addr) {return addr;});
 
 function getCPU() {
-    // refresh cpuCounter
+    //Refresh cpuCounter
     cpuCounter = stats.cpus();
     cpuCounter.forEach(function(e) {
         cpuIdle += e.times.idle;
         cpuTotal += e.times.user + e.times.nice + e.times.sys + e.times.idle + e.times.irq;
     })
 
-    // calculate difference between updated and previous values
+    //Calculate difference between updated and previous values
     cpuIdled = cpuIdle - cpuIdlep;
     cpuTotald = cpuTotal - cpuTotalp;
 
-    // attribuates new values to previous values
+    //Attribuates new values to previous values
     cpuIdlep = cpuIdle;
     cpuTotalp = cpuTotal;
 
-    // calculate CPU usage in percentage, with 2 decimals
+    //Calculate CPU usage in percentage, with 2 decimals
     cpuPercent = (((cpuTotald - cpuIdled)/cpuTotald)*100).toFixed(2);
 }
 
 function getRAM(send) {
-    // refresh ramCounter
+    //Refresh ramCounter
     ramCounter = stats.mem();
     ramPercent = ((ramCounter.used / ramCounter.total)*100).toFixed(2);
 }
 
 function publishData() {
-    // update CPU and RAM values
+    //Update CPU and RAM values
     getCPU();
     getRAM();
     date = new Date();
 
-    // send datas to the broker
+    //Send datas to the broker
     client.publish('tenodata', '{"mac":"'+mac+'", "ip":"'+ip+'", "hours":"'+date+'","cpu": "'+cpuPercent+'", "ram":"'+ramPercent+'"}');
 
-    // callback
+    //Callback
     setTimeout(function() {publishData();}, 1000);
 }
 
+//MQTT Publisher initialize
 client.on('connect', () => {
-    client.publish('test', 'New publisher !');
+    client.publish('tenodata', 'New publisher !');
 })
 
+//Repeat getCPU()
 getCPU();
 setTimeout(function() {publishData();}, 1000);
